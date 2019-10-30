31f0a8cac9910308e9fe166ab44e081dd4b4b44a
ruiquan.dai <ruiquan.dai@cwdata.com>
2019-10-30 17:51:45 +0800

    完成

--- src/index.js
+++ src/index.js
@@ -52,30 +52,51 @@ function Square(props) {
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
-        return ;
+        return;
       }
       squares[i] = this.state.xIsNext ? 'X':'O';
       this.setState({ // 定义了这个方法才会重新渲染。
         history: history.concat([{
-          squares: squares,
+          squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
 
+    jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 
+        '跳转到：' + move:
+        '重新开始';
+        return (
+          <li   key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
       
       let status;
       if (winner) {
@@ -88,12 +109,12 @@ function Square(props) {
           <div className="game-board">
             <Board
               squares={current.squares}
-              onClick={(i) => this.handleClick(i)}
+              onClick={i => this.handleClick(i)}
             />
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
